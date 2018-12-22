d249ee017dc9109e566bde00c6a42b3d1e507da6
Administrator <Administrator@SC-201804192124>
2018-12-22 20:54:20 +0800

    WeaveApplication.java

--- src/main/java/com/gl/weave/WeaveApplication.java
+++ src/main/java/com/gl/weave/WeaveApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan(value="com.gl.weave.dao")
 public class WeaveApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WeaveApplication.class, args);
 	}
